a3ecdcb80a40c931f5b2ebb843050890fd19e731
docente.dev <docente.dev@gmail.com>
2022-06-17 03:57:51 -0400

    feat: Added helth check

--- pages/api/health.ts
+++ pages/api/health.ts
@@ -2,12 +2,19 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
-  name: string
+  uptime: number,
+  message: string,
+  date: Date
 }
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  const data = {
+    uptime: process.uptime(),
+    message: 'Ok',
+    date: new Date()
+  }
+  res.status(200).json(data)
 }
